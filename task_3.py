b6adfb87ec4f39278a0dbf31c6f9af644a2d560d
Oksana Smirnova <oksana23smirnova@gmail.com>
2026-07-20 20:15:40 +0300

    Aftr second review

--- task_3.py
+++ task_3.py
@@ -4,11 +4,11 @@ class PointsForPlace:
     def get_points_for_place(place):
         points = 0
         if place > 100:
-            return 'Баллы начисляются только первым 100 участникам'
+            print('Баллы начисляются только первым 100 участникам')
         elif place < 1:
-            return 'Спортсмен не может занять нулевое или отрицательное место'
+            print('Спортсмен не может занять нулевое или отрицательное место')
         points = 101 - place
-        return int(points)
+        return points
 
 
 class PointsForMeters:
@@ -17,9 +17,9 @@ class PointsForMeters:
     def get_points_for_meters(meters):
         points = 0
         if meters < 0:
-            return 'Количество метров не может быть отрицательным'
+            print('Количество метров не может быть отрицательным')
         points = meters * 0.5
-        return int(points)
+        return points
 
 
 class TotalPoints(PointsForPlace, PointsForMeters):
